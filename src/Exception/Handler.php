84bee07abf20714ad14838d143c7852f59da8d00
Josh Harrison <josh@picklewagon.com>
2016-11-10 23:37:08 -0700

    Format exceptions to return json

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -4,9 +4,33 @@ namespace Picklewagon\ApiResponse\Exception;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Picklewagon\ApiResponse\Contract\Debug\MessageBagErrors;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
+    /**
+     * Generic response format.
+     *
+     * @var array
+     */
+    protected $format;
+
+    /**
+     * Indicates if we are in debug mode.
+     *
+     * @var bool
+     */
+    protected $debug = false;
+
+    /**
+     * User defined replacements to merge with defaults.
+     *
+     * @var array
+     */
+    protected $replacements = [];
+
     /**
      * Report or log an exception.
      *
@@ -31,6 +55,206 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        //return parent::render($request, $exception);
+        return $this->handle($exception);
+    }
+
+    /**
+     * Handle an exception if it has an existing handler.
+     *
+     * @param \Exception $exception
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handle(Exception $exception)
+    {
+        return $this->genericResponse($exception);
+    }
+
+    /**
+     * Handle a generic error response if there is no handler available.
+     *
+     * @param \Exception $exception
+     *
+     * @throws \Exception
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected function genericResponse(Exception $exception)
+    {
+        $replacements = $this->prepareReplacements($exception);
+
+        $response = $this->newResponseArray();
+
+        array_walk_recursive($response, function (&$value, $key) use ($exception, $replacements) {
+            if (starts_with($value, ':') && isset($replacements[$value])) {
+                $value = $replacements[$value];
+            }
+        });
+
+        $response = $this->recursivelyRemoveEmptyReplacements($response);
+
+        return new Response($response, $this->getStatusCode($exception), $this->getHeaders($exception));
+    }
+
+    /**
+     * Get the status code from the exception.
+     *
+     * @param \Exception $exception
+     *
+     * @return int
+     */
+    protected function getStatusCode(Exception $exception)
+    {
+        return $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : 500;
+    }
+
+    /**
+     * Get the headers from the exception.
+     *
+     * @param \Exception $exception
+     *
+     * @return array
+     */
+    protected function getHeaders(Exception $exception)
+    {
+        return $exception instanceof HttpExceptionInterface ? $exception->getHeaders() : [];
+    }
+
+    /**
+     * Prepare the replacements array by gathering the keys and values.
+     *
+     * @param \Exception $exception
+     *
+     * @return array
+     */
+    protected function prepareReplacements(Exception $exception)
+    {
+        $statusCode = $this->getStatusCode($exception);
+
+        if (! $message = $exception->getMessage()) {
+            $message = sprintf('%d %s', $statusCode, Response::$statusTexts[$statusCode]);
+        }
+
+        $replacements = [
+            ':message' => $message,
+            ':status_code' => $statusCode,
+        ];
+
+        if ($exception instanceof MessageBagErrors && $exception->hasErrors()) {
+            $replacements[':errors'] = $exception->getErrors();
+        }
+
+        if ($code = $exception->getCode()) {
+            $replacements[':code'] = $code;
+        }
+
+        if ($this->runningInDebugMode()) {
+            $replacements[':debug'] = [
+                'line' => $exception->getLine(),
+                'file' => $exception->getFile(),
+                'class' => get_class($exception),
+                'trace' => explode("\n", $exception->getTraceAsString()),
+            ];
+        }
+
+        return array_merge($replacements, $this->replacements);
+    }
+
+    /**
+     * Set user defined replacements.
+     *
+     * @param array $replacements
+     *
+     * @return void
+     */
+    public function setReplacements(array $replacements)
+    {
+        $this->replacements = $replacements;
+    }
+
+    /**
+     * Recursirvely remove any empty replacement values in the response array.
+     *
+     * @param array $input
+     *
+     * @return array
+     */
+    protected function recursivelyRemoveEmptyReplacements(array $input)
+    {
+        foreach ($input as &$value) {
+            if (is_array($value)) {
+                $value = $this->recursivelyRemoveEmptyReplacements($value);
+            }
+        }
+
+        return array_filter($input, function ($value) {
+            if (is_string($value)) {
+                return ! starts_with($value, ':');
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Create a new response array with replacement values.
+     *
+     * @return array
+     */
+    protected function newResponseArray()
+    {
+        if ($this->format == null) {
+            $this->format = config('api-response.errorFormat');
+        }
+
+        return $this->format;
+    }
+
+    /**
+     * Get the exception status code.
+     *
+     * @param \Exception $exception
+     * @param int        $defaultStatusCode
+     *
+     * @return int
+     */
+    protected function getExceptionStatusCode(Exception $exception, $defaultStatusCode = 500)
+    {
+        return ($exception instanceof HttpExceptionInterface) ? $exception->getStatusCode() : $defaultStatusCode;
+    }
+
+    /**
+     * Determines if we are running in debug mode.
+     *
+     * @return bool
+     */
+    protected function runningInDebugMode()
+    {
+        return $this->debug;
+    }
+
+    /**
+     * Set the error format array.
+     *
+     * @param array $format
+     *
+     * @return void
+     */
+    public function setErrorFormat(array $format)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * Set the debug mode.
+     *
+     * @param bool $debug
+     *
+     * @return void
+     */
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
     }
 }
